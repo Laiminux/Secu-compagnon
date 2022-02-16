ac9c8d9d63f5b135260159b620a74ec3f9eb0c3a
Laiminux <mathieu.lucas85@outlook.fr>
2022-02-16 12:05:26 +0000

    oeoe

--- pgs/PG2.c
+++ pgs/PG2.c
@@ -20,7 +20,57 @@
 #include <stdio.h>
 
 int main(){
-	printf
+	while(1){
+		printf("\n Choisissez entre pierre ( p ), feuille ( f ) et ciseaux ( c ) : \n");
+		char c;
+		scanf("%c",&c);
+		int x = rand()%3;
+		char k; 
+		switch(x){
+			case 1 : k='f';
+					break;
+			case 2: k='p';
+					break;
+			case 0: k='c';
+					break; 
+		}
+		if(c=='f'){
+			switch(k){
+			case 'f' : printf("draw \n");
+                                        break;
+                        case 'p': printf("win \n");
+                                        break;
+                        case 'c': printf("lose \n");
+                                        break;
+
+			}
+		}
+		else if(c=='p'){
+                        switch(k){
+                        case 'p' : printf("draw \n");
+                                        break;
+                        case 'c': printf("win \n");
+                                        break;
+                        case 'f': printf("lose \n");
+                                        break;
+
+                        }
+                }
+		else if(c=='c'){
+                        switch(k){
+                        case 'c' : printf("draw \n");
+                                        break;
+                        case 'f': printf("win \n");
+                                        break;
+                        case 'p': printf("lose \n");
+                                        break;
+
+                        }
+                }
+
+
+	}
+	return 0;
 }
 
 
